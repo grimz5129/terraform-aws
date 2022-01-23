898dda51ffadf0698c576de402799a5333c84120
Yefri Velasquez <59632166+grimz5129@users.noreply.github.com>
2022-01-23 23:23:02 +0000

    Add files via upload

---
+++ sakila-infra/output.tf
@@ -0,0 +1,3 @@
+# output "output_sakila_vpc_id_tf" {
+#   value = aws_vpc.java10x_sakila_yvelasquez_vpc_tf.id
+# }
---
+++ sakila-infra/webserver/output.tf
@@ -0,0 +1,3 @@
+output "output_sakila_rt_public_id_tf" {
+  value = aws_route_table.java10x_sakila_yvelasquez_rt_public_tf.id
+}
